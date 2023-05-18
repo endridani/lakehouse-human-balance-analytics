c94c250219dfd1de2d1260a98fe829806ee81fb3
endridani <endri.dani@gmail.com>
2023-05-18 13:36:11 +0200

    Create table for step_trainer_landing

---
+++ table_scripts/step_trainer_landing.sql
@@ -0,0 +1,10 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`step_trainer_landing` (
+  `sensorReadingTime` bigint,
+  `serialNumber` string,
+  `distanceFromObject` int
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES ('serialization.format' = '1')
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://ed-lake-house/step_trainer/landing/'
+TBLPROPERTIES ('classification' = 'json');
