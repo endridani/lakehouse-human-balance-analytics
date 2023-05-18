a7f32cc47e808c469409b6976a5a49705c256796
endridani <endri.dani@gmail.com>
2023-05-18 15:10:50 +0200

    Updated accelerometer_landing_to_trusted to drop duplicates

--- glue_jobs/accelerometer_landing_to_trusted.py
+++ glue_jobs/accelerometer_landing_to_trusted.py
@@ -4,6 +4,8 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql import functions as SqlFuncs
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -63,9 +65,16 @@ DropFields_node1684343813851 = DropFields.apply(
     transformation_ctx="DropFields_node1684343813851",
 )
 
+# Script generated for node Drop Duplicates
+DropDuplicates_node1684415377135 = DynamicFrame.fromDF(
+    DropFields_node1684343813851.toDF().dropDuplicates(),
+    glueContext,
+    "DropDuplicates_node1684415377135",
+)
+
 # Script generated for node Accelerometer Trusted
 AccelerometerTrusted_node3 = glueContext.write_dynamic_frame.from_options(
-    frame=DropFields_node1684343813851,
+    frame=DropDuplicates_node1684415377135,
     connection_type="s3",
     format="json",
     connection_options={
